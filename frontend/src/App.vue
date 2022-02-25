b2af801a44fcbc3e5ce3a84349981630f2650214
KaniKim <kkh5428@gmail.com>
2022-02-25 20:20:02 +0900

    add new practice

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-    <current-time />
+    <current-time class="col-4" />
+    <task-input class="col-6" @add-task="addNewTask" />
   </div>
 </template>
 
 <script>
 import CurrentTime from "./components/CurrentTime.vue";
+import TaskInput from "./components/TaskInput.vue";
 
 export default {
   name: "app",
   components: {
     CurrentTime,
+    TaskInput,
+  },
+  methods: {
+    addNewTask(task) {
+      alert(`New task added: ${task}`);
+    },
   },
 };
 </script>
