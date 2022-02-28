4195efd30a29bd4872850b2f27c8a51a17dea516
KaniKim <kkh5428@gmail.com>
2022-02-28 09:19:02 +0900

    rebuild vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,89 +1,30 @@
 <template>
-  <div id="app">
-    <current-time class="col-4" />
-    <task-input class="col-6" @add-task="addNewTask" />
-    <div class="col-12">
-      <div class="cardBox">
-        <div class="container">
-          <h2>My Tasks</h2>
-          <ul class="taskList">
-            <li
-              v-for="(taskItem, index) in displayList"
-              :key="`${index}_${Math.random()}`"
-            >
-              <input
-                type="checkbox"
-                :checked="!!taskItem.finishedAt"
-                @input="changeStatus(index)"
-              />
-              {{ taskItem.task }}
-              <span v-if="taskItem.finishedAt">
-                Done at: {{ formatDate(taskItem.finishedAt) }}</span
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<script>
-import CurrentTime from "./components/CurrentTime.vue";
-import TaskInput from "./components/TaskInput.vue";
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "TodoApp",
-  components: {
-    CurrentTime,
-    TaskInput,
-  },
-  data: () => ({
-    taskList: [],
-  }),
-  computed: {
-    displayList() {
-      return this.taskList;
-    },
-  },
-  methods: {
-    formatDate(value) {
-      if (!value) return "";
-      if (typeof value !== "number") return value;
+nav {
+  padding: 30px;
+}
 
-      const browserLocale =
-        navigator.languages && navigator.languages.length
-          ? navigator.languages[0]
-          : navigator.language;
-      const intlDateTime = new Intl.DateTimeFormat(browserLocale, {
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-      });
-      return intlDateTime.format(new Date(value));
-    },
-    addNewTask(task) {
-      this.taskList.push({
-        task,
-        createdAt: Date.now(),
-        finishedAt: undefined,
-      });
-    },
-    changeStatus(taskIndex) {
-      const task = this.taskList[taskIndex];
-      if (task.finishedAt) {
-        task.finishedAt = undefined;
-      } else {
-        task.finishedAt = Date.now();
-      }
-    },
-  },
-};
-</script>
-<style scoped>
-.taskList li {
-  text-align: left;
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Vuelidate from "vuelidate";
-import "./style.css";
 
-createApp(App).use(store).use(router).use(Vuelidate).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
