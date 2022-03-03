d9a0913ed288277fc79bbd650da37af724b2f481
KaniKim <kkh5428@gmail.com>
2022-03-03 10:13:15 +0900

    complete model of user

--- backend/main.py
+++ backend/main.py
@@ -1,89 +1,40 @@
 import datetime
-from typing import Optional
 
-import crud
-from schemas import TokenData, User
-
-from sqlalchemy.orm import Session
 from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from pydantic import BaseSettings
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from schemas import User
 
+import security
+from schemas import Token
 
-class Settings(BaseSettings):
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-
-    class Config:
-        env_file = ".env"
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-settings = Settings()
 app = FastAPI()
 
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def authenticate_user(db: Session, user_id: int, password: str):
-    user = crud.get_user(user_id=user_id)
+@app.post("/token", response_model=Token)
+async def login_for_access_token(db: Session, form_data: OAuth2PasswordRequestForm = Depends()):
+    user = security.authenticate_user(db, form_data.username, form_data.password)
 
     if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
-
-def create_access_token(data: dict, expires_delta: Optional[datetime.timedelta] = None):
-    to_encode = data.copy()
-
-    if expires_delta:
-        expire = datetime.datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
-
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-
-    return encoded_jwt
-
-
-async def get_current_user(db: Session, token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detial="Could  not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
+    access_token_expires = datetime.timedelta(minutes=security.settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = security.create_access_token(
+        data={"sub": user.user_id}, expires_delta=access_token_expires
     )
 
-    try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        user_id: int = payload.get("sub")
-
-        if user_id is None:
-            raise credentials_exception
-        token_data = TokenData(user_id=user_id)
-    except JWTError:
-        raise credentials_exception
-
-    user = crud.get_user(db, user_id=user_id)
-    if user is None:
-        raise credentials_exception
-
-    return user
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+@app.get("/users/me", response_model=User)
+async def read_users_me(current_user: User = Depends(security.get_current_active_user)):
     return current_user
+
+
+@app.get("/users/me/itmes/")
+async def read_own_items(current_user: User = Depends(security.get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.user_id}]
---
+++ backend/security.py
@@ -0,0 +1,88 @@
+import datetime
+from typing import Optional
+
+import crud
+from schemas import TokenData, User
+
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    class Config:
+        env_file = ".env"
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+settings = Settings()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(db: Session, user_id: int, password: str):
+    user = crud.get_user(user_id=user_id)
+
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[datetime.timedelta] = None):
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+    return encoded_jwt
+
+
+async def get_current_user(db: Session, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detial="Could  not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id: int = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+        token_data = TokenData(user_id=user_id)
+    except JWTError:
+        raise credentials_exception
+
+    user = crud.get_user(db, user_id=user_id)
+    if user is None:
+        raise credentials_exception
+
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
