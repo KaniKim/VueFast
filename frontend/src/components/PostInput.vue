1b735632b2661d6f02ea18139223d9d428d78209
KaniKim <kkh5428@gmail.com>
2022-03-11 08:34:58 +0900

    add new textarea

--- frontend/src/components/PostInput.vue
+++ frontend/src/components/PostInput.vue
@@ -1,10 +1,38 @@
 <template>
   <v-container fluid>
-    <v-textarea
-      clearable
-      clear-icon="mdi-close-circle"
-      label="Text"
-      value="This is clearable text."
-    ></v-textarea>
+    <v-row align="center" justify="center">
+      <v-col cols="8" lg="8">
+        <v-card tile>
+          <br />
+          <br />
+          <v-row align="center" justify="center">
+            <v-col cols="8" lg="8">
+              <v-textarea
+                label="title"
+                no-resize
+                rows="1"
+                value="title"
+              ></v-textarea>
+            </v-col>
+            <v-col cols="8" lg="8">
+              <v-textarea
+                clearable
+                clear-icon="mdi-close-circle"
+                label="content"
+                value="This is clearable text."
+              ></v-textarea>
+            </v-col>
+            <v-col cols="8" lg="8">
+              <v-textarea
+                label="tags"
+                no-resize
+                rows="1"
+                value="title"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
