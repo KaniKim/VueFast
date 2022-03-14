d3cac8bd9602e9d3964c4550b77ded84d570c319
KaniKim <kkh5428@gmail.com>
2022-03-14 09:37:12 +0900

    add tag dynamically

--- frontend/src/components/PostInput.vue
+++ frontend/src/components/PostInput.vue
@@ -7,28 +7,26 @@
           <br />
           <v-row align="center" justify="center">
             <v-col cols="8" lg="8">
-              <v-textarea
-                label="title"
-                no-resize
-                rows="1"
-                value="title"
-              ></v-textarea>
+              <v-text-field label="title" no-resize rows="1"></v-text-field>
             </v-col>
             <v-col cols="8" lg="8">
               <v-textarea
                 clearable
                 clear-icon="mdi-close-circle"
                 label="content"
-                value="This is clearable text."
               ></v-textarea>
             </v-col>
             <v-col cols="8" lg="8">
-              <v-textarea
-                label="tags"
-                no-resize
-                rows="1"
-                value="title"
-              ></v-textarea>
+              <v-text-field v-model="currentInput" @keypress.enter="saveChip">
+              </v-text-field>
+              <v-chip-group>
+                <v-chip v-for="(chip, i) of chips" :key="chip.label"
+                  >{{ chip }}
+                  <v-icon @click="deleteChip(i)" small right>
+                    mdi-close-circle-outline
+                  </v-icon>
+                </v-chip>
+              </v-chip-group>
             </v-col>
           </v-row>
         </v-card>
@@ -36,3 +34,30 @@
     </v-row>
   </v-container>
 </template>
+<script>
+export default {
+  props: {
+    set: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      chips: [],
+      currentInput: "",
+    };
+  },
+  methods: {
+    saveChip() {
+      const { chips, currentInput, set } = this;
+      ((set && chips.indexOf(currentInput) === -1) || !set) &&
+        chips.push(currentInput);
+      this.currentInput = "";
+    },
+    deleteChip(index) {
+      this.chips.splice(index, 1);
+    },
+  },
+};
+</script>
