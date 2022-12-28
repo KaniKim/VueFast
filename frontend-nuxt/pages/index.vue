998b7fa5d9a2139f774f6d89e4a203a0e44e834a
Kani Kim <kkh5428@gmail.com>
2022-12-28 19:35:44 +0900

    add nuxt

---
+++ frontend-nuxt/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <BarDrawer/>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import BarDrawer from "./components/LayoutComponent/NavBar";
+</script>
