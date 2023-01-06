5fb6956bd3b1ca3b702b82ca25ee554c084d2301
Kani Kim <kkh5428@gmail.com>
2023-01-06 10:41:08 +0900

    add test code of user

--- backend/tests/factories/user.py
+++ backend/tests/factories/user.py
@@ -1,10 +1,19 @@
+import random
+
 from faker import Faker
 
 from dto.user import User
 
 
 class UserFactory:
+    Faker.seed(seed=random.seed(777))
     factory = Faker()
 
     def create_user(self):
-        return User(name=self.factory.user_name(), email=self.factory.email)
+        return User(
+            name=self.factory.name(),
+            email=self.factory.email(),
+            password=self.factory.password(),
+            created_at=self.factory.date_time_ad(),
+            updated_at=self.factory.date_time_ad(),
+        )
--- backend/tests/user/test_user.py
+++ backend/tests/user/test_user.py
@@ -1,15 +1,34 @@
 import pytest
-from fastapi import FastAPI
 from httpx import AsyncClient
-from starlette.status import HTTP_201_CREATED
+from starlette.status import HTTP_201_CREATED, HTTP_200_OK
+
+from tests.factories.user import UserFactory
 
 
 class TestUserRoutes:
+
+    user = UserFactory().create_user()
+
     @pytest.mark.asyncio
-    async def test_user_creates(self, app: FastAPI, client: AsyncClient) -> None:
+    async def test_user_creates(self, client: AsyncClient) -> None:
+
         res = await client.post(
             "/api/user/",
-            json={"email": "kan1i@gmail.com", "password": "123456", "name": "name"},
+            json={
+                "email": self.user.email,
+                "password": self.user.password,
+                "name": self.user.name,
+            },
         )
 
+        data = res.json()
+
         assert res.status_code == HTTP_201_CREATED
+        assert data["email"] == self.user.email
+        assert data["name"] == self.user.name
+
+    @pytest.mark.asyncio
+    async def test_user_get(self, client: AsyncClient) -> None:
+        res = await client.get(f"/api/user/?email={self.user.email}")
+
+        assert res.status_code == HTTP_200_OK
